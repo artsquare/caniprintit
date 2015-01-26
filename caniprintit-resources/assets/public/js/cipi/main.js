392100c3fbdf6a858a2b8799a1b1300bf8553f32
Arik Gadye <arikdovgadye@gmail.com>
2015-01-26 11:56:19 -0800

    add initial flow from form elements to figuring out print-sizes

--- caniprintit-resources/assets/public/js/cipi/main.js
+++ caniprintit-resources/assets/public/js/cipi/main.js
@@ -1,14 +1,31 @@
-define(['jquery', 'bacon', 'bacon.jquery'], function($, bacon, bjq) {	
+define(['jquery', 'bacon', 'bacon.jquery', 'printanalyzer/findBestAR', 'printanalyzer/AspectRatio', 'vow'], function($, bacon, bjq, findBestAR, AspectRatio, vow) {	
 	$(document).ready(function() {
-		var width = $('#widthInput').asEventStream('keyup').map(function(e) {
-			return parseInt($(e.target).val());
+
+		//reading from form changes
+		var width = bjq.textFieldValue($('#widthInput')).changes().map(function(e) {
+			return parseInt(e)
 		});
-		var height = $('#heightInput').asEventStream('keyup').map(function(e) {
-			return parseInt($(e.target).val());
-		});
+		var height = bjq.textFieldValue($('#heightInput')).changes().map(function(e) {
+			return parseInt(e);
+		})
+		
 		var both = bacon.combineAsArray(width, height);
 		both.onValue(function(val) {
-			console.log(val)
-		})
+			return new vow.Promise(function(resolve, reject){
+				if (val[0] > 0 && val[1] > 0) {
+					resolve(findBestAR(val[0], val[1]));
+				} else {
+					reject(new Error("Bad response" + this.status));
+				}
+			}).then(function(AR) {
+				if (AR) {
+					return AR.ratio.findSizes({width: val[0], height: val[1]}, 130, 150);
+				} else {
+					console.log("Bad dimensions");
+				}
+			}).then(function(sizes) {
+				console.log(sizes);
+			});
+		});
 	});
 });
