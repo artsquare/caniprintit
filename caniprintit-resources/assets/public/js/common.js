75a606a1815d0fdd01faac99db425028b6afb542
Arik Gadye <arikdovgadye@gmail.com>
2015-01-26 00:32:01 -0800

    add bacon, bacon.jquery and bacon.model to require config

--- caniprintit-resources/assets/public/js/common.js
+++ caniprintit-resources/assets/public/js/common.js
@@ -3,7 +3,10 @@ require.config({
     paths: {
         'jquery': '//code.jquery.com/jquery-2.1.3.min',
         'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min',
-        'vow': 'vendor/vow/lib/vow'
+        'vow': 'vendor/vow/lib/vow',
+        'bacon': 'vendor/bacon/dist/Bacon',
+        'bacon.jquery': 'vendor/bacon.jquery/dist/bacon.jquery',
+        'bacon.model': 'vendor/bacon.model/dist/bacon.model'
     },
     shim: {
         'bootstrap': {
