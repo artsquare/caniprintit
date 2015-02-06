b950a9683d48c7388188ad255d906f89ddc558a9
Christopher Smith <chrylis@gmail.com>
2015-02-05 20:50:08 -0600

    add file-size collection and strip `image/` prefix

--- caniprintit-resources/assets/public/js/cipi/keenImage.js
+++ caniprintit-resources/assets/public/js/cipi/keenImage.js
@@ -1,11 +1,12 @@
 define(['cipi/keenConfig'], function(keenClient) {
     'use strict';
 
-    return function(width, height, imageType, isMobile, userAgent) {
+    return function(width, height, imageType, fileSize, isMobile, userAgent) {
         keenClient.addEvent(window.keenCollection, {
             width: width,
             height: height,
             imageType: imageType,
+            fileSize: fileSize,
             isMobile: isMobile,
             userAgent: userAgent
         });
